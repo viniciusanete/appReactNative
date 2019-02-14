3f13eb80fb5105d80b49424758013b5f5c9e022c
Vinicius Anete <viniciusanete@gmail.com>
2019-02-13 23:07:04 -0200

    implementando ScrollView e FlatList

--- InstaluraMobile/App.js
+++ InstaluraMobile/App.js
@@ -8,43 +8,47 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, 
+        StyleSheet, 
+        Text, 
+        View,
+        Image,
+        Dimensions,
+        ScrollView,
+        FlatList
+      } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
+const width = Dimensions.get('screen').width;
 type Props = {};
 export default class App extends Component<Props> {
   render() {
+
+    const fotos = [
+      {id:1, usuario: 'Vinícius'},
+      {id:2, usuario: 'Thainan'},
+      {id:3, usuario: 'Cintia'}
+    ]
+    
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <FlatList style={{marginTop: 20}}
+        keyExtractor={item => item.id}
+        data={fotos}
+        renderItem= { ({item}) =>
+          <View key={item.id}>
+            <Text>{item.usuario}</Text>
+            <Image source={require('./resources/img/rosa.jpg')} style={{width: width, height: width}}/>
+          </View>
+        }
+      />
+
+      // <ScrollView style={{marginTop: 20}}>
+      //   {fotos.map(foto =>
+      //     <View key={foto.id}>
+      //       <Text>{foto.usuario}</Text>
+      //       <Image source={require('./resources/img/rosa.jpg')} style={{width: width, height: width}}/>
+      //     </View>
+      //   )}
+      // </ScrollView>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
